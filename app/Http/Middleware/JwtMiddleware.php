8394666a95031078a5d8f251abba26a7ea6f0857
Ayush Gupta <ayushag320@gmail.com>
2024-06-20 14:42:49 +0530

    Correct the JwtMiddleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -3,21 +3,15 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\Models\User;
+use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Illuminate\Http\Request;
-use Firebase\JWT\ExpiredException;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class JwtMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         $token = $request->header('Authorization');
 
@@ -26,21 +20,19 @@ class JwtMiddleware
         }
 
         try {
-            $key=env('JWT_SECRET');
-            $credentials = JWT::decode($token,new Key($key,'HS256'));
-        } catch (ExpiredException $e) {
-            return response()->json(['error' => 'Token expired'], 401);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Invalid token'], 401);
+            $credentials = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+            $user = \App\Models\User::find($credentials->user_id);
+
+            if (!$user) {
+                return response()->json(['error' => 'User not found'], 404);
+            }
+
+            // Set the authenticated user
+            Auth::setUser($user);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Invalid token', 'message' => $e->getMessage()], 401);
         }
-
-        $user = User::find($credentials->user_id);
-
-        if (!$user) {
-            return response()->json(['error' => 'User not found'], 404);
-        }
-
-        // $request->auth = $user;
 
         return $next($request);
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\Router;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Router $router): void
     {
-        //
+        $router->aliasMiddleware('jwt', JwtMiddleware::class);
     }
 }
